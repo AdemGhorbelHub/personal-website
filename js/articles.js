d567c45552e57d372b2ae8b176151f4e670feaef
AdemGhorbelHub <adem_ghorbel@yahoo.com>
2025-01-02 11:57:01 +0100

    Update articles.js

--- js/articles.js
+++ js/articles.js
@@ -1,1 +1,19 @@
+// Load articles from JSON
+fetch('data/articles.json')
+    .then(response => response.json())
+    .then(data => {
+        const articlesGrid = document.getElementById('articlesGrid');
+        data.forEach(article => {
+            const articleCard = document.createElement('div');
+            articleCard.classList.add('article-card');
 
+            const content = `
+                <img src="${article.image}" alt="${article.title}">
+                <h3>${article.title}</h3>
+                <p>${article.summary}</p>
+                <a href="${article.link}" target="_blank">Read More</a>
+            `;
+            articleCard.innerHTML = content;
+            articlesGrid.appendChild(articleCard);
+        });
+    });
