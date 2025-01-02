0f1d6ce0258ab16e1b749e6a86f3f818458be1c8
AdemGhorbelHub <adem_ghorbel@yahoo.com>
2025-01-02 11:52:02 +0100

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,1 +1,19 @@
+// Load gallery images from JSON
+fetch('data/gallery.json')
+    .then(response => response.json())
+    .then(data => {
+        const galleryGrid = document.getElementById('galleryGrid');
+        data.forEach(image => {
+            const galleryItem = document.createElement('div');
+            galleryItem.classList.add('gallery-item');
 
+            const content = `
+                <img src="${image.src}" alt="${image.alt}">
+                <div class="overlay">
+                    <p>${image.caption}</p>
+                </div>
+            `;
+            galleryItem.innerHTML = content;
+            galleryGrid.appendChild(galleryItem);
+        });
+    });
